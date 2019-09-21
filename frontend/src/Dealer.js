769c9bb75547c73fcada0affba1a3a21fba7edae
Francesco Bedussi <francesco.bedussi@abstract.it>
2019-09-21 16:41:06 +0200

    feat: new dealer look

--- frontend/src/Dealer.js
+++ frontend/src/Dealer.js
@@ -1,99 +1,124 @@
 import React, { Component } from "react";
-
+import QRCode from "qrcode.react";
 import io from "socket.io-client";
 
 class Dealer extends Component {
-    state = {
-        connected: false,
-        number: null,
-        clients: [],
-        countdown: null,
-        winner: null,
-        clientId: ""
-    };
-    componentDidMount() {
-        const socket = io({
-            path: "/lottery",
-            query: {
-                dealer: true
-            }
-        });
+  state = {
+    connected: false,
+    number: null,
+    clients: [],
+    countdown: null,
+    winner: null,
+    clientId: "",
+    counting: false
+  };
+  componentDidMount() {
+    const socket = io({
+      path: "/lottery",
+      query: {
+        dealer: true
+      }
+    });
 
-        socket.on("connect", () => {
-            this.setState({ connected: true, clientId: this.socket.id });
-        });
+    socket.on("connect", () => {
+      this.setState({ connected: true, clientId: this.socket.id });
+    });
 
-        socket.on("reconnect_attempt", () => {
-            socket.io.opts.query = {
-                dealer: true
-            };
-        });
+    socket.on("reconnect_attempt", () => {
+      socket.io.opts.query = {
+        dealer: true
+      };
+    });
 
-        socket.on("updatelist", clients => {
-            this.setState({ clients });
-        });
-        socket.on("countdown", countdown => {
-            console.log(countdown);
-            this.setState({ countdown });
-        });
-        socket.on("result", ({ winner }) => {
-            console.log(winner);
-            this.setState({ winner: socket.id === winner, counting: false });
-        });
+    socket.on("updatelist", clients => {
+      this.setState({ clients });
+    });
+    socket.on("countdown", countdown => {
+      console.log(countdown);
+      this.setState({ countdown });
+    });
+    socket.on("result", ({ winner }) => {
+      console.log(winner);
+      this.setState({ winner, counting: false });
+    });
 
-        socket.on("startCounting", () => {
-            console.log("start countdown");
-            this.setState({ winner: null, counting: true });
-        });
+    socket.on("startCounting", () => {
+      console.log("start countdown");
+      this.setState({ winner: null, counting: true });
+    });
 
-        // socket.on("event", this.onSocketData.bind(this));
-        socket.on("disconnect", () => {
-            this.setState({ connected: false });
-        });
+    // socket.on("event", this.onSocketData.bind(this));
+    socket.on("disconnect", () => {
+      this.setState({ connected: false });
+    });
 
-        this.socket = socket;
-    }
+    this.socket = socket;
+  }
 
-    onSocketData(data) {
-        console.log("Socket", data);
-    }
+  onSocketData(data) {
+    console.log("Socket", data);
+  }
 
-    startCount() {
-        console.log("Start");
-        this.socket.emit("start");
-    }
+  startCount() {
+    console.log("Start");
+    this.socket.emit("start");
+  }
 
-    render() {
-        return (
-            <div>
-                {!this.state.connected && <div>Connecting...</div>}
-                {this.state.connected && (
-                    <div>
-                        <p>{this.state.clients[this.state.countdown]}</p>
+  getRandomBackgroundColor() {
+    return `hsl(${Math.round(Math.random() * 360)}, 60%, 60%)`;
+  }
 
-                        <button
-                            type="button"
-                            onClick={() => this.startCount()}
-                            disabled={this.state.counting}
-                        >
-                            START
-                        </button>
-
-                        <p>
-                            {Object.keys(this.state.clients).length}{" "}
-                            Partecipants
-                        </p>
-
-                        <ul>
-                            {Object.keys(this.state.clients).map(id => (
-                                <li key={id}>{this.state.clients[id]}</li>
-                            ))}
-                        </ul>
-                    </div>
-                )}
+  render() {
+    return (
+      <div>
+        {!this.state.connected && <div>Connecting...</div>}
+        {this.state.connected && (
+          <div className={`dealer-deck ${
+            this.state.winner ? "is-winner" : ""
+          }`}>
+            <div
+              className={`dealer-deck__box`}
+              style={
+                this.state.counting
+                  ? { backgroundColor: this.getRandomBackgroundColor() }
+                  : {}
+              }
+            >
+              {this.state.counting || this.state.winner ? (
+                <div className="dealer-deck__box__number">
+                  {
+                    this.state.clients[
+                      this.state.winner || this.state.countdown
+                    ]
+                  }
+                </div>
+              ) : (
+                <QRCode
+                  className="qr-code"
+                  renderAs="svg"
+                  value={window.location.href.replace("/dealer", "")}
+                />
+              )}
             </div>
-        );
-    }
+
+            <button
+              className="start-btn"
+              type="button"
+              onClick={() => this.startCount()}
+              disabled={this.state.counting}
+            >
+              START
+            </button>
+
+            <p className="deler-deck__text">
+              {Object.keys(this.state.clients).length} Partecipants
+            </p>
+
+          </div>
+        )}
+      </div>
+    );
+  }
 }
 
 export default Dealer;
--- frontend/src/Main.js
+++ frontend/src/Main.js
@@ -3,80 +3,81 @@ import React, { Component } from "react";
 import io from "socket.io-client";
 
 class Main extends Component {
-    state = {
-        connected: false,
-        number: null,
-        clients: [],
-        countdown: null,
-        winner: null,
-        clientId: ""
-    };
-    componentDidMount() {
-        // const socket = io.apply("http://localhost:3000/lottery");
+  state = {
+    connected: false,
+    number: null,
+    clients: [],
+    countdown: null,
+    winner: null,
+    clientId: ""
+  };
+  componentDidMount() {
+    // const socket = io.apply("http://localhost:3000/lottery");
 
-        const socket = io({
-            path: "/lottery"
-        });
+    const socket = io({
+      path: "/lottery"
+    });
 
-        socket.on("connect", () => {
-            this.setState({ connected: true, clientId: this.socket.id });
-        });
-        socket.on("updatelist", clients => {
-            this.setState({ clients });
-        });
-        socket.on("countdown", countdown => {
-            this.setState({ countdown });
-        });
-        socket.on("result", ({ winner }) => {
-            this.setState({ winner: socket.id === winner, counting: false });
-        });
+    socket.on("connect", () => {
+      this.setState({ connected: true, clientId: this.socket.id });
+    });
+    socket.on("updatelist", clients => {
+      this.setState({ clients });
+    });
+    socket.on("countdown", countdown => {
+      this.setState({ countdown });
+    });
+    socket.on("result", ({ winner }) => {
+      this.setState({ winner: socket.id === winner, counting: false });
+    });
 
-        socket.on("startCounting", () => {
-            this.setState({ winner: null, counting: true });
-        });
+    socket.on("startCounting", () => {
+      this.setState({ winner: null, counting: true });
+    });
 
-        socket.on("disconnect", () => {
-            this.setState({ connected: false });
-        });
+    socket.on("disconnect", () => {
+      this.setState({ connected: false });
+    });
 
-        this.socket = socket;
-    }
+    this.socket = socket;
+  }
 
-    render() {
-        const playerClass = [
-            this.state.counting === true &&
-            this.state.clientId === this.state.countdown
-                ? "player-deck--current"
-                : "",
-            this.state.counting === false && this.state.winner === true
-                ? "player-deck--winner"
-                : this.state.counting === false && this.state.winner === false
-                    ? "player-deck--looser"
-                    : ""
-        ].join(" ");
+  render() {
+    const playerClass = [
+      this.state.counting === true &&
+      this.state.clientId === this.state.countdown
+        ? "player-deck--current"
+        : "",
+      this.state.counting === false && this.state.winner === true
+        ? "player-deck--winner"
+        : this.state.counting === false && this.state.winner === false
+        ? "player-deck--looser"
+        : ""
+    ].join(" ");
 
-        return (
-            <div className={`player-deck ${playerClass}`}>
-                {!this.state.connected && <div>Connecting...</div>}
-                {this.state.connected && (
-                    <div>
-                        <div className="player-deck__number">
-                            {this.state.clients ? (
-                                this.state.clients[this.state.clientId]
-                            ) : (
-                                <p>Getting number...</p>
-                            )}
-                        </div>
-
-                        <p>
-                            {Object.keys(this.state.clients).length}{" "}
-                            Partecipants
-                        </p>
-                    </div>
+    return (
+      <div>
+        {!this.state.connected && <div>Connecting...</div>}
+        {this.state.connected && (
+          <div className={`player-deck ${playerClass}`}>
+            <div className="player-deck__box">
+              <div className="player-deck__box__number">
+                {this.state.clients ? (
+                  this.state.clients[this.state.clientId]
+                ) : (
+                  <p>Getting number...</p>
                 )}
+              </div>
             </div>
-        );
-    }
+
+            <p className="player-deck__text">
+              {Object.keys(this.state.clients).length} Partecipants
+            </p>
+          </div>
+        )}
+      </div>
+    );
+  }
 }
 
 export default Main;
